3a291a932bdf2ae542e71004937d0caeedade8f5
Claus Wilke <wilke@austin.utexas.edu>
2016-07-01 00:19:25 -0500

    example scripts

---
+++ examples/GFP_de-CpG.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python3
+from codon_manipulator import SequenceAnalyzer, CodonOptimizer
+
+from Bio import SeqIO
+from Bio.Seq import Seq
+from Bio.Alphabet import IUPAC
+
+class StopAndCpGScorer:
+    def __init__(self, m2stop_coef, CpG_coef, UpA_coef):
+        self.sa = SequenceAnalyzer()
+        self.m2stop_coef = m2stop_coef
+        self.CpG_coef = CpG_coef
+        self.UpA_coef = UpA_coef
+
+    def calc_score_components(self, seq):
+        m2stop_count = self.sa.count_muts_to_stop(seq)
+        CpG_count, UpA_count = self.sa.count_CpG(seq)
+        return m2stop_count, CpG_count, UpA_count
+
+    def score(self, seq):
+        m2stop_count, CpG_count, UpA_count = self.calc_score_components(seq)
+        return self.m2stop_coef*m2stop_count + self.CpG_coef*CpG_count + self.UpA_coef*UpA_count
+    
+        
+def de_CpG(seq):
+    """Remove as many CpGs as possible, as well as potential single-point mutations to stop and UpAs.
+"""
+    scorer = StopAndCpGScorer(1, 1, 1)
+    o = CodonOptimizer(scorer)
+
+    seq_orig = seq
+    seq, score = o.hillclimb(seq, maximize=False, max_wait_count = 5000, verbosity=0)
+    assert seq_orig.translate() == seq.translate()
+    
+    m2stop_count, CpG_count, UpA_count = scorer.calc_score_components(seq_orig)
+    print("Original sequence")
+    print("=================")
+    print("Mutations to stop: %i, CpG count: %i, UpA count: %i" % (m2stop_count, CpG_count, UpA_count))
+
+    m2stop_count, CpG_count, UpA_count = scorer.calc_score_components(seq)    
+    print("\nOptimized sequence:")
+    print("=================")
+    print("Mutations to stop: %i, CpG count: %i, UpA count: %i\n" % (m2stop_count, CpG_count, UpA_count))
+    print(seq)
+    
+        
+# when run as its own script, 
+if __name__ == "__main__":
+    # read in GFP wt sequence    
+    seq = SeqIO.parse(open("GFP_wt.fasta", "rU"), "fasta").__next__().seq
+    # run analysis    
+    de_CpG(seq)
---
+++ examples/GFP_muts2stop.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+from codon_manipulator import CodonAnalyzer, reverse_genetic_code
+
+from Bio import SeqIO
+from Bio.Seq import Seq
+    
+def analyze_m2stop(seq):
+    print("""Each line contains one codon of wt GFP, the corresponding amino acid,
+and the number of single-point mutations that lead to a stop codon.
+For codons for which the count exceeds 0, all possible alternative codons
+and their counts are shown as well.""")
+
+    ca = CodonAnalyzer()
+
+    for i in range(int(len(seq)/3)):
+        codon = seq[3*i:3*i+3]
+        count = ca.count_stop_muts(codon)
+        aa = str(codon.translate())
+        print(codon, aa, count)
+        if count > 0:
+            all_codons = reverse_genetic_code[aa]
+            print("alternative codons for " + aa + ":")
+            for c in all_codons:
+                print(" ", c, ca.count_stop_muts(c))
+            
+        
+# when run as its own script, 
+if __name__ == "__main__":
+    # read in GFP wt sequence    
+    seq = SeqIO.parse(open("GFP_wt.fasta", "rU"), "fasta").__next__().seq
+    # run analysis
+    analyze_m2stop(seq)
