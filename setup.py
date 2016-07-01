cf0541a70d59a5e66b1c28e3467390a4eb1a8cfb
Claus Wilke <wilke@austin.utexas.edu>
2016-06-30 22:32:07 -0500

    reorg

--- setup.py
+++ setup.py
@@ -3,8 +3,11 @@ from setuptools import setup
 setup(name='codon_manipulator',
       version='0.1',
       description='Toolkit to manipulate synonymous codon usage in various ways',
-      url='http://github.com/storborg/funniest',
+      url='http://github.com/clauswilke/codon_manipulator',
       author='Claus O. Wilke',
       author_email='wilke@austin.utexas.edu',
       license='MIT',
-      packages=['codon_manipulator'])
+      package_dir = {'codon_manipulator':'src'},
+      install_requires=['Biopython'],
+      packages=['codon_manipulator']
+      )
---
+++ src/__init__.py
@@ -0,0 +1,4 @@
+__version__ = '0.1'
+from .codon_analyzer.py	import *
+from .sequence_analyzer.py import *
+from .sequence_analyzer.py import *
