a662cfa2f48a2eb31bc1c3d5079e3731eaa7dd27
Benjamin Jack <benjamin.r.jack@gmail.com>
2017-01-12 16:56:11 -0600

    Added helper function for counting differences between two sequences.

--- src/sequence_analyzer.py
+++ src/sequence_analyzer.py
@@ -32,30 +32,30 @@ Note: The code expects Ts not Us in the sequence.
                     CpG_count += 1
             if seq[i] == 'T':
                 if seq[i+1] == 'A':
-                    UpA_count += 1                    
+                    UpA_count += 1
         return (CpG_count, UpA_count)
-        
+
     def count_muts_to_stop(self, seq):
         """Counts the number of possible single-point mutations to a stop codon.
 """
         assert len(seq) % 3 == 0
-        
+
         count = 0
         for i in range(int(len(seq)/3)):
             codon = seq[3*i:3*i+3]
             count += self.codon_analyzer.count_stop_muts(codon)
         return count
-        
+
     def calc_Fop(self, seq, opt_codons = opt_codons_E_coli, verbosity = 0):
         """Calculates the fraction of optimal codons in a sequence. The fraction is calculated relative to the number of sites where an optimal codon is possible (i.e., for example, methionines are excluded since they are coded by only one amino acid).
 """
         assert len(seq) % 3 == 0
-        
+
         codon_list = [j for i in opt_codons.values() for j in i]
         aa_list = opt_codons.keys()
         total_count = 0
         opt_count = 0
-        
+
         for i in range(int(len(seq)/3)):
             codon = seq[3*i:3*i+3]
             aa = str( codon.translate() )
@@ -67,7 +67,7 @@ Note: The code expects Ts not Us in the sequence.
                 opt = (c in codon_list)
             total_count += not ignore
             opt_count += opt
-        
+
             if verbosity > 1:
                 print(3*i, aa, str( codon ), end=' ')
                 if ignore:
@@ -102,7 +102,7 @@ Note: The code expects Ts not Us in the sequence.
         """Counts how many times each codon appears in a sequence, and returns a dictionary with codon counts.
 """
         assert len(seq) % 3 == 0
-        
+
         counts = self.zero_codon_counts.copy()
         for i in range(int(len(seq)/3)):
             codon = str(seq[3*i:3*i+3])
@@ -119,7 +119,7 @@ Note: The code expects Ts not Us in the sequence.
             outfile.write('--Absolute counts--\n')
             self.print_codon_freqs(counts, outfile)
 
-        
+
         # normalize by amino-acid family
         for aa in reverse_genetic_code:
             if aa == '*': # we skip over stop codons
@@ -130,9 +130,17 @@ Note: The code expects Ts not Us in the sequence.
             if total > 0:
                 for c in fam_counts:
                     counts[c] = fam_counts[c]/total
-        
+
         if verbosity > 0:
             if verbosity > 1:
                 outfile.write('--Relative frequencies--\n')
             self.print_codon_freqs(counts, outfile)
         return counts
+
+    def count_diffs(self, seq1, seq2):
+        """Count the number of differences between two sequences of the same
+        length."""
+        assert len(seq1) == len(seq2)
+        diffs = sum(1 for a, b in zip(seq1, seq2) if a != b)
+
+        return diffs
