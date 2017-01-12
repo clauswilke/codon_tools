6b90b1c2ee00bc7b9a3aee4adbebda533f72b954
Benjamin Jack <benjamin.r.jack@gmail.com>
2017-01-12 14:27:00 -0600

    Added CodonShuffler to shuffle codons without changing codon freqs.

---
+++ src/codon_shuffler.py
@@ -0,0 +1,51 @@
+import random
+
+from Bio.Seq import Seq
+from Bio.Alphabet import IUPAC, generic_dna
+
+class CodonShuffler:
+    """
+    Class to shuffle codons within an ORF, without changing the overall
+    codon frequency.
+    """
+    def __init__(self):
+        pass
+
+    def make_lookup_table(self, seq):
+        """
+        Make a reverse codon lookup table that lists codons used in seq for
+        each amino acid. The same codon may be listed more than once for each
+        amino acid.
+        """
+
+        codons = {}
+
+        seq = Seq(seq, generic_dna)
+
+        for i in range(int(len(seq)/3)):
+            codon = seq[3*i:3*i+3]
+            amino_acid = str(Seq.translate(codon))
+            if amino_acid in codons:
+                codons[amino_acid].append(str(codon))
+            else:
+                codons[amino_acid] = [str(codon)]
+
+        return codons
+
+    def shuffle_codons(self, seq):
+        """
+        Shuffle codons in a sequence without changing codon frequencies.
+        """
+
+        codons = self.make_lookup_table(seq)
+
+        for amino_acid in codons:
+            random.shuffle(codons[amino_acid])
+
+        seq_aa = Seq(seq, generic_dna).translate()
+        shuffled_seq = ""
+
+        for amino_acid in str(seq_aa):
+            shuffled_seq += codons[amino_acid].pop()
+
+        return shuffled_seq
