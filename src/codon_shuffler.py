32562b0497e1e1025806fb96765665c4089b6645
Benjamin Jack <benjamin.r.jack@gmail.com>
2017-01-12 16:19:13 -0600

    Added start_window and end_window support to CodonShuffler.

--- src/codon_shuffler.py
+++ src/codon_shuffler.py
@@ -30,20 +30,28 @@ class CodonShuffler:
 
         return codons
 
-    def shuffle_codons(self, seq):
+    def shuffle_codons(self, seq, start_window = 0, end_window = 0):
         """
         Shuffle codons in a sequence without changing codon frequencies.
         """
+        # Trim sequence based on start and end windows
+        start = start_window*3
+        end = len(seq) - end_window*3
+        seq_trim = seq[start:end]
+
+        # Make sure sequence doesn't have any partial codons
+        assert len(seq_trim) % 3 == 0
+
         # Construct dictionary of all codons present in the sequence
-        codons = self.make_lookup_table(seq)
+        codons = self.make_lookup_table(seq_trim)
         # Shuffle codons
         for amino_acid in codons:
             random.shuffle(codons[amino_acid])
         # Translate original sequence
-        seq_aa = Seq(seq, generic_dna).translate()
-        shuffled_seq = ""
+        seq_aa = Seq(seq_trim, generic_dna).translate()
+        shuffled_seq = seq[0:start]
         # Reconstruct sequence with codons shuffled
         for amino_acid in str(seq_aa):
             shuffled_seq += codons[amino_acid].pop()
-
+        shuffled_seq += seq[end:]
         return shuffled_seq
